2b653cf057f2e3c65f5fd9dd6ca5de189f50ecb5
Mostafa Anter <ga30000@gmail.com>
2016-02-07 23:50:56 +0200

    splash&titleBar

---
+++ app/src/main/java/com/cap/social/socialcap/splash/SplashScreen.java
@@ -0,0 +1,69 @@
+package com.cap.social.socialcap.splash;
+
+import android.content.Intent;
+
+
+import com.cap.social.socialcap.MainActivity;
+import com.cap.social.socialcap.R;
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+/**
+ * Created by mostafa on 03/02/16.
+ */
+public class SplashScreen extends AwesomeSplash {
+
+    //DO NOT OVERRIDE onCreate()!
+    //if you need to start some services do it in initSplash()!
+
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+            /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+        configSplash.setPathSplash(LogoPath.logo_path); //set path String
+        configSplash.setOriginalHeight(555); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(615); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.primary_text); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.primary_light); //path object filling color
+
+
+        //Customize Title
+        configSplash.setTitleSplash("socialCap");
+        configSplash.setTitleTextColor(R.color.primary_light);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+        configSplash.setTitleFont("fonts/gordon.ttf"); //provide string to your font located in assets/fonts/
+
+    }
+
+    @Override
+    public void animationsFinished() {
+
+        //transit to another activity here
+        //or do whatever you want
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+    }
+}
